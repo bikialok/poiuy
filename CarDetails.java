18ade1fe6a5614648c495437edd7340ae3bf0cf4
Alok <vicky.alok93@gmail.com>
2023-04-02 01:02:53 +0530

    Hi team added user carrier details

---
+++ CarDetails.java
@@ -0,0 +1,34 @@
+public class   CarDetails{
+           public static void main(String[] args){
+                // declaration of data type;
+                String model ="KIA CELTOS HTE 1.5";
+                String company = "KIA South Korea";
+                String color ="RED/BLUE/SILVER";
+                double price = 10.69;
+                double milege = 16.5;
+                int engineCapacity = 1497;
+                String seatingCapacity ="5 Seater";
+                int door = 6;
+                String fuelType = "Petrol";
+                int fuelCapacity = 50;
+                String transmission = "Manual- 6 Gear";
+                String emissionStandard ="BS6";
+                String safety ="6 - Airbags";
+
+                System.out.println("Model Name ::"+model);
+                System.out.println("Company Name ::"+company);
+                System.out.println("Colors Available ::"+color);
+                System.out.println("Price in Lakh ::"+price);
+                System.out.println("Milege kmpl ::"+milege);
+                System.out.println("Engine Capacity in CC ::"+engineCapacity);
+                System.out.println("Seating Capacity ::"+seatingCapacity);
+                System.out.println("Doors::"+door);
+                System.out.println("Fuel Type ::"+fuelType);
+                System.out.println("Fuel Capacity in L ::"+fuelCapacity);
+                System.out.println("Transmission ::"+transmission);
+                System.out.println("Emission Standard ::"+emissionStandard);
+                System.out.println("Safety Features ::"+safety);
+
+                
+    } 
+}
